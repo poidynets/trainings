1bf65465c58dfdfa138142d237a043972acb108e
Vasa <35889432+poidynets@users.noreply.github.com>
2018-07-06 17:29:16 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Remoting.Messaging;
-using System.Text;
-using System.Threading.Tasks;
+using System;
+
 
 namespace Trainings
 {
@@ -11,22 +7,19 @@ namespace Trainings
     {
         static void Main(string[] args)
 
-        /*
-
-        Имя, возраст, здоровье и состояние жив / мертв будут описывать каждый конкретный экземпляр нашего класса -
-        на основе этих параметров так же будет основываться работа функций Talk, Go, Kill и IsAliveOrNot. 
-        Имя будет передаваться в класс, во время вызова конструктора класса. Возраст и состояние здоровья будут 
-        генерироваться в конструкторе автоматически - в дальнейшем они будут влиять на вывод текста в консоль, при вызове доступных пользователю методов.
-
-        */
+            /*
+    We are creating human with name, health, and it will be possible to give a commands to speak, walk, die and to find out alive it or not.
+    Human name will be sent to the class during using class constructor. Age and heals status are generating in the constructor automatically.
+    Later theyll take a part in next steps related displaying text regarding human during using available methods.
+            */
         {
             string user_command = "";
             bool Infinity = true;
             Human SomeHuman = null;
             while (Infinity)
             {
-                System.Console.WriteLine("Waiting for your command... \n");
-                user_command = System.Console.ReadLine();
+                Console.WriteLine("Waiting for your command... \n");
+                user_command = Console.ReadLine();
 
                 switch (user_command)
                 {
@@ -37,29 +30,29 @@ namespace Trainings
                     }
                     case "help":
                     {
-                        System.Console.WriteLine("\nList of commands");
-                        System.Console.WriteLine("---");
-                        System.Console.WriteLine("'create_human' : to create a human");
-                        System.Console.WriteLine("'exit' : to close from App");
-                        System.Console.WriteLine("'kill_human' : to kill human");
-                        System.Console.WriteLine("'talk' : function forcing to speak (if person is creahed)");
-                        System.Console.WriteLine("'go' : forcing to walk  (if person is creahed)");
-                        System.Console.WriteLine("---");
-                        System.Console.WriteLine("--- \n");
+                        Console.WriteLine("\nList of commands");
+                        Console.WriteLine("---");
+                        Console.WriteLine("'create_human' : to create a human");
+                        Console.WriteLine("'exit' : to close from App");
+                        Console.WriteLine("'kill_human' : to kill human");
+                        Console.WriteLine("'talk' : function forcing to speak (if person is creahed)");
+                        Console.WriteLine("'go' : forcing to walk  (if person is creahed)");
+                        Console.WriteLine("---");
+                        Console.WriteLine("--- \n");
 
                         break;
                     }
 
                     case "create_human":
                     {
-                        if (SomeHuman !=null)
+                        if (SomeHuman != null)
                         {
                             SomeHuman.Kill();
                         }
-                        System.Console.WriteLine("Please set a name of created human \n");
-                        user_command = System.Console.ReadLine();
+                        Console.WriteLine("Please set a name of created human \n");
+                        user_command = Console.ReadLine();
                         SomeHuman = new Human(user_command);
-                        System.Console.WriteLine("Human is successfully created \n");
+                        Console.WriteLine("Human is successfully created \n");
                         break;
                     }
 
@@ -72,7 +65,7 @@ namespace Trainings
                         }
                         else
                         {
-                            System.Console.WriteLine("Human person is not created. You cant complete kill func");
+                            Console.WriteLine("Human person is not created. You cant complete kill func");
                         }
                         break;
 
@@ -85,7 +78,7 @@ namespace Trainings
                         }
                         else
                         {
-                            System.Console.WriteLine("Human person is not created. Function can't be processed");
+                            Console.WriteLine("Human person is not created. Function can't be processed");
                         }
                         break;
 
@@ -98,15 +91,15 @@ namespace Trainings
                         }
                         else
                         {
-                            System.Console.WriteLine("Human person is not created. Function can't be processed");
+                            Console.WriteLine("Human person is not created. Function can't be processed");
                         }
                         break;
                     }
                     default:
                     {
-                        System.Console.WriteLine("we dont have this function \n");
-                        System.Console.WriteLine("write 'help' function to display the list of the functions");
-                        System.Console.WriteLine("write 'exit' for App finish \n\n");
+                        Console.WriteLine("we dont have this function \n");
+                        Console.WriteLine("write 'help' function to display the list of the functions");
+                        Console.WriteLine("write 'exit' for App finish \n\n");
                         break;
 
                     }
@@ -116,83 +109,82 @@ namespace Trainings
     }
 
     public class Human
+    {
+        public Human(string _name)
         {
-            public Human(string _name)
+            Name = _name;
+            isAlive = true;
+            Age = (uint)rnd.Next(15, 51);
+            Health = (uint)rnd.Next(10, 101);
+        }
+
+        private string Name;
+        private uint Age;
+        private uint Health;
+        private bool isAlive;
+        private Random rnd = new Random();
+
+        public void Talk()
+        {
+            int random_talk = rnd.Next(1, 4);
+            string tmp_str = "";
+
+            switch (random_talk)
             {
-                Name = _name;
-                isAlive = true;
-                Age = (uint) rnd.Next(15, 51);
-                Health = (uint) rnd.Next(10, 101);
-            }
-
-            private string Name;
-            private uint Age;
-            private uint Health;
-            private bool isAlive;
-            private Random rnd = new Random();
-
-            public void Talk()
-            {
-                int random_talk = rnd.Next(1, 4);
-                string tmp_str = "";
-
-                switch (random_talk)
+                case 1:
                 {
-                    case 1:
-                    {
-                        tmp_str = "Name" + Name + "cheers";
-                        break;
-                    }
-                    case 2:
-                    {
-                        tmp_str = "Im" + Age + "Not to old?";
-                        break;
-                    }
-                    case 3:
-                    {
-                        if (Health > 50)
-                            tmp_str = "Im so healthy!";
-                        else
-                            tmp_str = "My health is so-so. Want to leave at least till" + (Age + 5).ToString();
-                        break;
-                    }
+                    tmp_str = "Name" + Name + "cheers";
+                    break;
                 }
-                System.Console.WriteLine(tmp_str);
-            }
-
-            public void Go()
-            {
-                if (isAlive == true)
+                case 2:
                 {
-                    if (Health > 40)
-                    {
-                        string outString = Name + "walknig in the park and planning evening";
-                        System.Console.WriteLine(outString);
-                    }
+                    tmp_str = "Im" + Age + "Not to old?";
+                    break;
+                }
+                case 3:
+                {
+                    if (Health > 50)
+                        tmp_str = "Im so healthy!";
                     else
-                    {
-                        string outString = Name + " he's sick and dont thinking about girls or even boys";
-                        System.Console.WriteLine(outString);
-                    }
+                        tmp_str = "My health is so-so. Want to leave at least till" + (Age + 5).ToString();
+                    break;
+                }
+            }
+            Console.WriteLine(tmp_str);
+        }
+
+        public void Go()
+        {
+            if (isAlive == true)
+            {
+                if (Health > 40)
+                {
+                    string outString = Name + "walknig in the park and planning evening";
+                    Console.WriteLine(outString);
                 }
                 else
                 {
-                    string outString = Name + "im sorry, he is dead :(";
-                    System.Console.WriteLine(outString);
+                    string outString = Name + " he's sick and dont thinking about girls or even boys";
+                    Console.WriteLine(outString);
                 }
             }
-
-
-            public bool IsAliveOrNot()
+            else
             {
-                return isAlive;
-            }
-
-            public void Kill()
-            {
-                isAlive = false;
-                System.Console.WriteLine(Name + " is died");
+                string outString = Name + "im sorry, he is dead :(";
+                Console.WriteLine(outString);
             }
         }
-    }
 
+
+        public bool IsAliveOrNot()
+        {
+            return isAlive;
+        }
+
+        public void Kill()
+        {
+            isAlive = false;
+            Console.WriteLine(Name + " is died");
+        }
+    }
+}
